41a567d14fc0d84d3b4d1ac322cf5f8914ad554b
Roman Hrubryn <115115965+spaidi96@users.noreply.github.com>
2023-03-22 01:04:20 +0200

    Initial commit

---
+++ Laba3_2/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+namespace Laba3_2
+{
+    public class Person
+    {
+        private string Name;
+        private int Age;
+
+        public Person(string value1, int value2)
+        {
+            Name = value1;
+            Age = value2;
+        }
+
+        public Person()
+        {
+            Name = null;
+            Age = 0;
+        }
+
+        ~Person()
+        {
+            Console.WriteLine("Об'єкт Person видалено");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+
+        public void SaveToFile(string filePath)
+        {
+            string json = JsonSerializer.Serialize(this);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static Person LoadFromFile(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Person>(json);
+        }
+    }
+}
+    
+    
---
+++ Laba3_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+namespace Laba3_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Person pers1 = new Person("John", 35);
+            Console.WriteLine("Екземпляр класу з допомогою конструктора з вхідними параметрами");
+            pers1.Display();
+            Console.WriteLine("-----------------------");
+            Person pers2 = new Person();
+            Console.WriteLine("Екземпляр класу з допомогою конструктора за замовчуванням");
+            pers2.Display();
+            Console.WriteLine("-----------------------");
+            pers1.SaveToFile("pers.json");
+            Person persjson= Person.LoadFromFile("pers.json");
+            persjson.Display();
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
